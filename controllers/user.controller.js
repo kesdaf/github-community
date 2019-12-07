f6238899daf13c11d709101462e6fd5f8c796991
Alberto Hernández <albertohdezherrera@gmail.com>
2019-12-07 10:17:51 +0100

    - Creacion de vistas login - Estilo de etiquetas basicas

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const User = require('../models/user.model');
 
 module.exports.login = (req, res, next) => {
-  res.render('index')
+  res.render('login/index')
 }
 
 module.exports.dologin = (req, res, next) => {
