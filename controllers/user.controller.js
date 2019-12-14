a14db4915edf9c7a1d2ca00baf8d43e0e9c6d7b4
Alberto Hernández <albertohdezherrera@gmail.com>
2019-12-14 15:45:58 +0100

    crear usuario & maquetacion

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -19,25 +19,47 @@ module.exports.logOut = (req, res, next) => {
 module.exports.profile = (req, res, next) => {
   res.render('login/profile', { user: req.session.user })
 }
+module.exports.new = (req, res, next) => {
+  res.render('login/profile', { new: true })
+}
+
 module.exports.updateProfile = (req, res, next) => {
 
-  req.file ? req.body.avatar = req.file.url : false
+  req.file ? req.body.avatar = req.file.url : 'img/avatar.png'
   !req.body.name ? delete req.body.name : false
   !req.body.email ? delete req.body.email : false
   !req.body.username ? delete req.body.username : false
   !req.body.password ? delete req.body.password : false
 
-  console.log(req.body)
-
-  User.findByIdAndUpdate(req.session.user._id, req.body, { new: true })
-    .then(usr => {
-      req.session.user = usr
-      res.redirect('/')
-    })
-    .catch(err => {
-      console.log(err)
-    })
-
+    if (req.session.user) {
+      User.findByIdAndUpdate(req.session.user._id, req.body, { new: true })
+      .then(usr => {
+        req.session.user = usr
+        res.redirect('/')
+      })
+      .catch(err => {
+        console.log('error => ' + err)
+      })
+    } else {
+      const user = new User({
+        username: req.body.username,
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        avatar: req.body.avatar
+      })
+      user.save()
+      .then((newUser) => {
+        res.redirect('/login')
+      })
+      .catch(error => {
+        if (error instanceof mongoose.Error.ValidationError) {
+          res.render('login/profile', { user, error: error.errors, new: true })
+        } else {
+          next(error);
+        }
+      })
+    }
 }
 
 module.exports.doLoginSocial = (req, res, next) => {
--- models/user.model.js
+++ models/user.model.js
@@ -39,6 +39,7 @@ const userSchema = new mongoose.Schema({
   },
   avatar: {
     type: String,
+    default: 'img/avatar.png'
   },
   languages:
   {
